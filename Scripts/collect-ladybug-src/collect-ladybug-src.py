bae7b4107082b2605847ee0e0725c36c2b3c995f
Arun Sharma <arun@sharma-home.net>
2025-11-21 12:15:08 -0800

    Update collect-ladybug-src with the new location

--- Scripts/collect-ladybug-src/collect-ladybug-src.py
+++ Scripts/collect-ladybug-src/collect-ladybug-src.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 INDENTATION = "    "*4
 
-LADYBUG = "Sources/Ladybug"
+LADYBUG = "Sources/LadybugCpp"
 LADYBUG_TARGET = "ladybug"
 CXX_LADYBUG = "cxx-ladybug"
 PACKAGE_SWIFT = "Package.swift"
